9dbd7bb63961bee53546c78a41e674535617530b
matius.kristian <matius.kristian@vidio.com>
2025-12-02 16:53:40 +0700

    HTML escaping

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # app.py
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "<h1>Hello from Flask in a Docker container!</h1>"
+
+# HTML Escaping
+@app.route('/login')
+def login():
+    test = request.args.get("name", "Flask")
+    return f"Hello, {escape(test)}!"
