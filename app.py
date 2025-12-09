2596319fcc8902cb8105968ff41646ad514e2464
mattivityroom <mattivityroom@gmail.com>
2025-12-09 15:37:27 +0700

    Variable rules

--- app.py
+++ app.py
@@ -13,3 +13,11 @@ def hello():
 def login():
     test = request.args.get("name", "Flask")
     return f"Hello, {escape(test)}!"
+
+@app.route('/user/<string:username>')
+def user_detail(username):
+    return f"Hello, {escape(username)}!"
+
+@app.route('/path/<path:subpath>')
+def show_path(subpath):
+    return f"Subpath {escape(subpath)}"
