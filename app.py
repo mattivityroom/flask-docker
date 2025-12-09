624118cb05218eca5774d91f626cfabe6c89eacb
mattivityroom <mattivityroom@gmail.com>
2025-12-09 15:50:42 +0700

    url for & http method

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 # app.py
-from flask import Flask, request
+from flask import Flask, request, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     return "<h1>Hello from Flask in a Docker container!</h1>"
 
-# HTML Escaping
-@app.route('/login')
+@app.get('/login')
 def login():
     test = request.args.get("name", "Flask")
     return f"Hello, {escape(test)}!"
 
 @app.route('/user/<string:username>')
 def user_detail(username):
-    return f"Hello, {escape(username)}!"
+    return f"{escape(username.capitalize())}'s user profile!"
 
 @app.route('/path/<path:subpath>')
 def show_path(subpath):
     return f"Subpath {escape(subpath)}"
+
+with app.test_request_context():
+    print(url_for('hello'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('user_detail', username='John Doe'))
+    print(url_for('show_path', subpath='a/b/c'))
